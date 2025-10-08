1d4f882b6397d5568f0ca776cbf545199895e01a
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-07 22:11:31 -0300

    adicionando rota principal para ver a api online

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,18 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '6mb' }));
   app.use(bodyParser.urlencoded({ limit: '6mb', extended: true }));
 
+  // Rota GET / para teste mais rebuscada
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.json({
+      status: 'success',
+      message: 'API Inscrição está funcionando corretamente ✅',
+      version: '1.0',
+      environment: process.env.NODE_ENV ?? 'development',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime().toFixed(2) + 's',
+    });
+  });
+
   const port = Number(process.env.PORT ?? 3000);
   await app.listen(port, '0.0.0.0');
 }
