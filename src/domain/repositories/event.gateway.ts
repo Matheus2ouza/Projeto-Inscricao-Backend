41e1acc0eea0bf90cb034fd74c16b773902433d6
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-16 03:51:16 -0300

    feat: enhance event management with new update methods
    - Added updateInscription and updatePayment methods to EventGateway for managing event status and payment updates.
    - Implemented corresponding methods in EventPrismaRepository to handle updates in the database.
    - Restored findById, findByRegion, and findByNameAndRegionId methods in EventPrismaRepository for improved event retrieval functionality.

--- src/domain/repositories/event.gateway.ts
+++ src/domain/repositories/event.gateway.ts
@@ -1,18 +1,21 @@
+import { statusEvent } from 'generated/prisma';
 import { Event } from '../entities/event.entity';
 
 export abstract class EventGateway {
   abstract create(event: Event): Promise<Event>;
+  abstract updateInscription(id: string, status: statusEvent): Promise<Event>;
+  abstract updatePayment(id: string, status: boolean): Promise<Event>;
   abstract findById(id: string): Promise<Event | null>;
   abstract findByRegion(regionId: string): Promise<Event[]>;
-  abstract findByNameAndRegionId(
-    name: string,
-    regionId: string,
-  ): Promise<Event | null>;
   abstract paymentEnabled(eventId: string): Promise<void>;
   abstract paymentDisabled(eventId: string): Promise<void>;
   abstract paymentCheck(eventId: string): Promise<boolean>;
   abstract update(event: Event): Promise<Event>;
   abstract delete(id: string): Promise<void>;
+  abstract findByNameAndRegionId(
+    name: string,
+    regionId: string,
+  ): Promise<Event | null>;
   abstract findAll(): Promise<Event[]>;
   abstract countTypesInscriptions(id: string): Promise<number>;
   abstract updateQuantityParticipants(
