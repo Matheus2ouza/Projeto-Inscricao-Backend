56fa7e72ddd9e2ab362811b8f9e805d247be4755
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-17 05:34:14 -0300

    feat: add EventTicket and TicketSale entities
    - Introduced EventTicket entity to manage event ticket details including creation, validation, and retrieval of ticket information.
    - Added TicketSale entity to handle ticket sales, including creation, validation, and tracking of sale details such as quantity and total value.
    - Enhanced data integrity with validation checks for both entities, ensuring proper ticket and sale management.

---
+++ src/domain/entities/ticket-sale.entity.ts
@@ -0,0 +1,122 @@
+import { StatusPayment } from 'generated/prisma';
+import { Utils } from 'src/shared/utils/utils';
+import { Entity } from '../shared/entities/entity';
+
+export type TicketSaleCreateDto = {
+  ticketId: string;
+  accountId: string;
+  quantity: number;
+  pricePerTicket: number;
+  status: StatusPayment;
+};
+
+export type TicketSaleWithDto = {
+  id: string;
+  ticketId: string;
+  accountId: string;
+  quantity: number;
+  totalValue: number;
+  status: StatusPayment;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export class TicketSale extends Entity {
+  private constructor(
+    id: string,
+    private ticketId: string,
+    private accountId: string,
+    private quantity: number,
+    private totalValue: number,
+    private status: StatusPayment,
+    createdAt: Date,
+    updatedAt: Date,
+  ) {
+    super(id, createdAt, updatedAt);
+    this.validate();
+  }
+
+  public static create({
+    ticketId,
+    accountId,
+    quantity,
+    status,
+    pricePerTicket,
+  }: TicketSaleCreateDto): TicketSale {
+    const id = Utils.generateUUID();
+    const createdAt = new Date();
+    const updatedAt = new Date();
+
+    const totalValue = quantity * pricePerTicket;
+
+    return new TicketSale(
+      id,
+      ticketId,
+      accountId,
+      quantity,
+      totalValue,
+      status,
+      createdAt,
+      updatedAt,
+    );
+  }
+
+  public static with({
+    id,
+    ticketId,
+    accountId,
+    quantity,
+    totalValue,
+    status,
+    createdAt,
+    updatedAt,
+  }: TicketSaleWithDto): TicketSale {
+    return new TicketSale(
+      id,
+      ticketId,
+      accountId,
+      quantity,
+      totalValue,
+      status,
+      createdAt,
+      updatedAt,
+    );
+  }
+
+  protected validate(): void {
+    if (this.quantity <= 0) {
+      throw new Error('A quantidade de tickets deve ser maior que zero');
+    }
+    if (this.totalValue < 0) {
+      throw new Error('O valor total não pode ser negativo');
+    }
+  }
+
+  public getTicketId(): string {
+    return this.ticketId;
+  }
+
+  public getAccountId(): string {
+    return this.accountId;
+  }
+
+  public getQuantity(): number {
+    return this.quantity;
+  }
+
+  public getTotalValue(): number {
+    return this.totalValue;
+  }
+
+  public getStatus(): StatusPayment {
+    return this.status;
+  }
+
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  public getUpdateAt(): Date {
+    return this.updatedAt;
+  }
+}
