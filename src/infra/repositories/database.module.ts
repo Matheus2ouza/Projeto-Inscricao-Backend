3930d0cd094ff3d33787dced59a7b3b54fc6d707
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-17 05:34:33 -0300

    feat: add EventTicket and TicketSale repository providers to DataBaseModule
    - Included EventTicketPrismaRepositoryProvider and TicketSalePrismaRepositoryProvider in the DataBaseModule to support event ticket and ticket sale functionalities.
    - Enhanced the module's exports to ensure proper access to the new repository providers for ticket management operations.

--- src/infra/repositories/database.module.ts
+++ src/infra/repositories/database.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CacheRecordRepositoryProvider } from './prisma/cache-record/cache-record.prisma.repository.provider';
+import { EventTicketPrismaRepositoryProvider } from './prisma/event-tickets/model/event-tickets.prisma.repository.provider';
 import { EventPrismaRepositoryProvider } from './prisma/event/model/event.prisma.repository.provider';
 import { FinancialMovementPrismaRepositoryProvider } from './prisma/financial-movement/model/financial-movement.repository.provider';
 import { InscriptionPrismaRepositoryProvider } from './prisma/inscription/model/inscription.prisma.repository.provider';
@@ -7,6 +8,7 @@ import { ParticipantPrismaRepositoryProvider } from './prisma/participant/model/
 import { PaymentInscriptionRepositoryProvider } from './prisma/payment-inscription/model/payment-inscription.prisma.repository.provider';
 import { PrismaService } from './prisma/prisma.service';
 import { RegionPrismaRepositoryProvider } from './prisma/region/region.prisma.repository.provider';
+import { TicketSalePrismaRepositoryProvider } from './prisma/ticket-sale/model/ticket-sale.prisma.repository.provider';
 import { TypeInscriptionPrismaRepositoryProvider } from './prisma/type-inscription/type-inscription.prisma.repository.provider';
 import { UserPrismaRepositoryProvider } from './prisma/user/model/user.prisma.repository.provider';
 
@@ -22,6 +24,8 @@ import { UserPrismaRepositoryProvider } from './prisma/user/model/user.prisma.re
     CacheRecordRepositoryProvider,
     PaymentInscriptionRepositoryProvider,
     FinancialMovementPrismaRepositoryProvider,
+    EventTicketPrismaRepositoryProvider,
+    TicketSalePrismaRepositoryProvider,
   ],
   exports: [
     PrismaService,
@@ -34,6 +38,8 @@ import { UserPrismaRepositoryProvider } from './prisma/user/model/user.prisma.re
     CacheRecordRepositoryProvider,
     PaymentInscriptionRepositoryProvider,
     FinancialMovementPrismaRepositoryProvider,
+    EventTicketPrismaRepositoryProvider,
+    TicketSalePrismaRepositoryProvider,
   ],
 })
 export class DataBaseModule {}
