ff029da8ec7aaaecbf1edc81a49e04e71a811d5a
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-24 04:46:46 -0300

    feat: introduce EventResponsibleGateway for event management
    - Added EventResponsibleGateway interface to define methods for managing event responsible entities, including creation, retrieval, and deletion operations.
    - This abstraction enhances the overall event management capabilities by standardizing interactions with event responsible data.

--- src/infra/repositories/prisma/event-expenses/model/mappers/event-expenses-prisma-model-to-event-expenses-entity.mapper.ts
+++ src/infra/repositories/prisma/event-expenses/model/mappers/event-expenses-prisma-model-to-event-expenses-entity.mapper.ts
@@ -2,9 +2,7 @@ import { EventExpenses } from 'src/domain/entities/event-expenses.entity';
 import EventExpensesPrismaModel from '../event-expenses.prisma.model';
 
 export class EventExpensesPrismaModelToEventExpensesEntityMapper {
-  public static map(
-    eventExpenses: EventExpensesPrismaModel,
-  ): EventExpenses {
+  public static map(eventExpenses: EventExpensesPrismaModel): EventExpenses {
     return EventExpenses.with({
       id: eventExpenses.id,
       eventId: eventExpenses.eventId,
