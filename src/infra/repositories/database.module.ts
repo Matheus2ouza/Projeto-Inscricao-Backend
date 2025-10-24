a86e2a5a7f02c2f37999d414fcf029f83a6b95ae
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-24 04:49:51 -0300

    feat: integrate EventResponsiblePrismaRepositoryProvider into database module
    - Added EventResponsiblePrismaRepositoryProvider to the database module for improved event management capabilities.
    - Updated import paths for TypeInscriptionPrismaRepositoryProvider to maintain consistency in the module structure.
    - Ensured proper formatting by adding a newline at the end of the client.prisma.ts file.

--- src/infra/repositories/database.module.ts
+++ src/infra/repositories/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CacheRecordRepositoryProvider } from './prisma/cache-record/cache-record.prisma.repository.provider';
 import { EventExpensesPrismaRepositoryProvider } from './prisma/event-expenses/model/event-expenses.prisma.repository.provider';
+import { EventResponsiblePrismaRepositoryProvider } from './prisma/event-responsibles/model/event-responsibles.prisma.repository.provider';
 import { EventTicketPrismaRepositoryProvider } from './prisma/event-tickets/model/event-tickets.prisma.repository.provider';
 import { EventPrismaRepositoryProvider } from './prisma/event/model/event.prisma.repository.provider';
 import { FinancialMovementPrismaRepositoryProvider } from './prisma/financial-movement/model/financial-movement.repository.provider';
@@ -13,7 +14,7 @@ import { PaymentInscriptionRepositoryProvider } from './prisma/payment-inscripti
 import { PrismaService } from './prisma/prisma.service';
 import { RegionPrismaRepositoryProvider } from './prisma/region/region.prisma.repository.provider';
 import { TicketSalePrismaRepositoryProvider } from './prisma/ticket-sale/model/ticket-sale.prisma.repository.provider';
-import { TypeInscriptionPrismaRepositoryProvider } from './prisma/type-inscription/type-inscription.prisma.repository.provider';
+import { TypeInscriptionPrismaRepositoryProvider } from './prisma/type-inscription/model/type-inscription.prisma.repository.provider';
 import { UserPrismaRepositoryProvider } from './prisma/user/model/user.prisma.repository.provider';
 
 @Module({
@@ -29,6 +30,7 @@ import { UserPrismaRepositoryProvider } from './prisma/user/model/user.prisma.re
     PaymentInscriptionRepositoryProvider,
     FinancialMovementPrismaRepositoryProvider,
     EventTicketPrismaRepositoryProvider,
+    EventResponsiblePrismaRepositoryProvider,
     TicketSalePrismaRepositoryProvider,
     OnSiteParticipantPrismaRepositoryProvider,
     OnSiteParticipantPaymentPrismaRepositoryProvider,
@@ -47,6 +49,7 @@ import { UserPrismaRepositoryProvider } from './prisma/user/model/user.prisma.re
     PaymentInscriptionRepositoryProvider,
     FinancialMovementPrismaRepositoryProvider,
     EventTicketPrismaRepositoryProvider,
+    EventResponsiblePrismaRepositoryProvider,
     TicketSalePrismaRepositoryProvider,
     OnSiteParticipantPrismaRepositoryProvider,
     OnSiteParticipantPaymentPrismaRepositoryProvider,
