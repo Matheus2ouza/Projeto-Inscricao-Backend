1e1ed373c85072bcc72ec03af27a110eba06761e
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-24 04:48:49 -0300

    fix: correct import statements and add missing commas in exception classes
    - Updated import statements in domain.exception.ts and validator-domain.exception.ts to use single quotes for consistency.
    - Added missing commas in the constructor parameters of both exception classes to ensure proper syntax.

--- src/domain/shared/exceptions/validator-domain.exception.ts
+++ src/domain/shared/exceptions/validator-domain.exception.ts
@@ -1,10 +1,10 @@
-import { DomainException } from "./domain.exception";
+import { DomainException } from './domain.exception';
 
 export class ValidatorDomainException extends DomainException {
   public constructor(
     internalMessage: string,
     externalMessage?: string,
-    context?: string
+    context?: string,
   ) {
     super(internalMessage, externalMessage, context);
     this.name = ValidatorDomainException.name;
