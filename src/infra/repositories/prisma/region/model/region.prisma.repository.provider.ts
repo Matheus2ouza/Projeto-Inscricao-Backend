94d7ca28fe8225617ba5a5e36221d78eb04efb2d
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-10-27 16:50:38 -0300

    feat: add Prisma repository providers for CacheRecord and Region
    - Introduced CacheRecordRepositoryProvider to integrate CacheRecordPrismaRepository with CacheRecordGateway.
    - Added RegionPrismaRepositoryProvider to connect RegionPrismaRepository with RegionGateway.

--- src/infra/repositories/prisma/region/model/region.prisma.repository.provider.ts
+++ src/infra/repositories/prisma/region/model/region.prisma.repository.provider.ts
@@ -1,5 +1,5 @@
-import { RegionPrismaRepository } from './region.prisma.repository';
 import { RegionGateway } from 'src/domain/repositories/region.gateway';
+import { RegionPrismaRepository } from '../region.prisma.repository';
 
 export const RegionPrismaRepositoryProvider = {
   provide: RegionGateway,
