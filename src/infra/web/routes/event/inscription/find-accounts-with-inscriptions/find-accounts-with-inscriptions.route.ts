a358777c7d9ae9c41faa3d1192ec195fec33585e
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-11-11 12:42:10 -0300

    feat: implement find accounts with inscriptions use case and related components
    - Added FindAccountWithInscriptionsUsecase to retrieve accounts associated with inscriptions for a specific event.
    - Created FindAccountWithInscriptionsInput and FindAccountWithInscriptionsOutput types for structured data handling.
    - Developed FindAccountWithInscriptionsPresenter to format the output for HTTP responses.
    - Implemented FindAccountWithInscriptionsRoute to manage incoming requests and return accounts with their inscriptions.
    - Introduced DTOs for response structure, enhancing API integration and data consistency.

---
+++ src/infra/web/routes/event/inscription/find-accounts-with-inscriptions/find-accounts-with-inscriptions.route.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import {
+  FindAccountWithInscriptionsInput,
+  FindAccountWithInscriptionsUsecase,
+} from 'src/usecases/event/inscription/find-accounts-with-inscriptions.usecase';
+import { FindAccountWithInscriptionsResponse } from './find-accounts-with-inscriptions.dto';
+import { FindAccountWithInscriptionsPresenter } from './find-accounts-with-inscriptions.presenter';
+
+@Controller('events')
+export class FindAccountWithInscriptionsRoute {
+  public constructor(
+    private readonly findAccountWithInscriptionsUsecase: FindAccountWithInscriptionsUsecase,
+  ) {}
+
+  @Get(':id/list-inscription')
+  async handle(
+    @Param('id') id: string,
+  ): Promise<FindAccountWithInscriptionsResponse> {
+    console.log('o eventId:', id);
+    const input: FindAccountWithInscriptionsInput = {
+      eventId: id,
+    };
+
+    const response =
+      await this.findAccountWithInscriptionsUsecase.execute(input);
+    return FindAccountWithInscriptionsPresenter.toHttp(response);
+  }
+}
