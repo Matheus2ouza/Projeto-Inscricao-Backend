c32d77fe523ca08fc44ace523588a2ed7c548038
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-11-11 12:39:45 -0300

    feat: implement find details event use case and related components
    - Added FindDetailsEventUsecase to retrieve detailed information about an event by its ID.
    - Created FindDetailsEventRequest and FindDetailsEventResponse DTOs for structured data handling.
    - Developed FindDetailsEventPresenter to format the output for HTTP responses.
    - Implemented FindDetailsEventRoute to handle incoming requests and return event details, ensuring proper API integration.

---
+++ src/infra/web/routes/event/find-details/find-details-event.presenter.ts
@@ -0,0 +1,23 @@
+import { FindDetailsEventOutput } from 'src/usecases/event/find-details/find-details-event.usecase';
+import { FindDetailsEventResponse } from './find-details-event.dto';
+
+export class FindDetailsEventPresenter {
+  public static toHttp(
+    output: FindDetailsEventOutput,
+  ): FindDetailsEventResponse {
+    return {
+      id: output.id,
+      name: output.name,
+      startDate: output.startDate,
+      endDate: output.endDate,
+      imageUrl: output.imageUrl,
+      location: output.location,
+      longitude: output.longitude,
+      latitude: output.latitude,
+      status: output.status,
+      paymentEnabled: output.paymentEnabled,
+      regionName: output.regionName,
+      typeInscriptions: output.typeInscriptions,
+    };
+  }
+}
