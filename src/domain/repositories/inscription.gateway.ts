402ac9efdb2e0cf9de3bf7800af2f2bc056fcc6d
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-11-12 14:36:52 -0300

    feat: add create and update methods to InscriptionGateway
    - Introduced abstract methods for creating and updating inscriptions in the InscriptionGateway, enhancing the interface for managing inscription entities.

--- src/domain/repositories/inscription.gateway.ts
+++ src/domain/repositories/inscription.gateway.ts
@@ -2,6 +2,8 @@ import { InscriptionStatus } from 'generated/prisma';
 import { Inscription } from '../entities/inscription.entity';
 
 export abstract class InscriptionGateway {
+  abstract create(inscription: Inscription): Promise<Inscription>;
+  abstract update(inscription: Inscription): Promise<Inscription>;
   abstract findById(id: string): Promise<Inscription | null>;
   abstract findByAccountId(accountId: string): Promise<Inscription[]>;
   abstract findByEventId(eventId: string): Promise<Inscription[]>;
@@ -9,7 +11,6 @@ export abstract class InscriptionGateway {
     eventId: string,
     accountIds: string[],
   ): Promise<Inscription[]>;
-  abstract create(inscription: Inscription): Promise<Inscription>;
   abstract findManyPaginated(
     page: number,
     pageSize: number,
