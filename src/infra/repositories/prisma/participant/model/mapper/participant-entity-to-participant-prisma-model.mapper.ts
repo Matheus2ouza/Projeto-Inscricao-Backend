bce77fd6eafedcec34823ced157bc9fc00ebee93
matheusDev <matheusfurtadogg@gmail.com>
2025-11-18 21:45:16 -0300

    Refactor ParticipantEntityToParticipantPrismaModelMapper to map Participant objects to ParticipantPrismaModel, improving code clarity and leveraging entity methods for data retrieval.

--- src/infra/repositories/prisma/participant/model/mapper/participant-entity-to-participant-prisma-model.mapper.ts
+++ src/infra/repositories/prisma/participant/model/mapper/participant-entity-to-participant-prisma-model.mapper.ts
@@ -2,22 +2,16 @@ import { Participant } from 'src/domain/entities/participant.entity';
 import ParticipantPrismaModel from '../participant.prisma.model';
 
 export class ParticipantEntityToParticipantPrismaModelMapper {
-  public static map(
-    participantPrisma: ParticipantPrismaModel & {
-      typeInscription?: { description?: string } | null;
-    },
-  ): Participant {
-    return Participant.with({
-      id: participantPrisma.id,
-      inscriptionId: participantPrisma.inscriptionId,
-      typeInscriptionId: participantPrisma.typeInscriptionId,
-      name: participantPrisma.name,
-      birthDate: participantPrisma.birthDate,
-      gender: participantPrisma.gender,
-      createdAt: participantPrisma.createdAt,
-      updatedAt: participantPrisma.updatedAt,
-      typeInscriptionDescription:
-        participantPrisma.typeInscription?.description ?? undefined,
-    });
+  public static map(participant: Participant): ParticipantPrismaModel {
+    return {
+      id: participant.getId(),
+      inscriptionId: participant.getInscriptionId(),
+      typeInscriptionId: participant.getTypeInscriptionId(),
+      name: participant.getName(),
+      birthDate: participant.getBirthDate(),
+      gender: participant.getGender(),
+      createdAt: participant.getCreatedAt(),
+      updatedAt: participant.getUpdatedAt(),
+    };
   }
 }
