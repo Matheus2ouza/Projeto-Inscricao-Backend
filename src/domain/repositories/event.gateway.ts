9a2ec49a5dbb8a8a1e7b611490250abe26622e1f
matheusDev <matheusfurtadogg@gmail.com>
2025-11-20 14:57:49 -0300

    Enhance EventGateway and EventPrismaRepository with additional CRUD methods and new event retrieval functionalities, including findNextUpcomingEvent and countEventsActive, for improved event management capabilities.

--- src/domain/repositories/event.gateway.ts
+++ src/domain/repositories/event.gateway.ts
@@ -2,17 +2,22 @@ import { statusEvent } from 'generated/prisma';
 import { Event } from '../entities/event.entity';
 
 export abstract class EventGateway {
+  // CRUD básico
   abstract create(event: Event): Promise<Event>;
-  abstract updateInscription(id: string, status: statusEvent): Promise<Event>;
-  abstract updatePayment(id: string, status: boolean): Promise<Event>;
-  abstract findById(id: string): Promise<Event | null>;
-  abstract findByRegion(regionId: string): Promise<Event[]>;
-  abstract paymentEnabled(eventId: string): Promise<void>;
-  abstract paymentDisabled(eventId: string): Promise<void>;
-  abstract paymentCheck(eventId: string): Promise<boolean>;
   abstract update(event: Event): Promise<Event>;
   abstract delete(id: string): Promise<void>;
   abstract updateImage(id: string, imageUrl: string): Promise<Event>;
+
+  // Atualizações de status e pagamento
+  abstract updateInscription(id: string, status: statusEvent): Promise<Event>;
+  abstract updatePayment(id: string, status: boolean): Promise<Event>;
+  abstract paymentEnabled(eventId: string): Promise<void>;
+  abstract paymentDisabled(eventId: string): Promise<void>;
+  abstract paymentCheck(eventId: string): Promise<boolean>;
+
+  // Buscas e listagens
+  abstract findById(id: string): Promise<Event | null>;
+  abstract findByRegion(regionId: string): Promise<Event[]>;
   abstract findByNameAndRegionId(
     name: string,
     regionId: string,
@@ -24,6 +29,18 @@ export abstract class EventGateway {
     page: number;
     pageSize: number;
   }): Promise<Event[]>;
+  abstract findAllCarousel(): Promise<
+    {
+      id: string;
+      name: string;
+      location: string;
+      imageUrl: string;
+    }[]
+  >;
+  abstract findNextUpcomingEvent(regionId: string): Promise<Event | null>;
+
+  // Agregações e contagens
+  abstract countEventsActive(regionId: string): Promise<number>;
   abstract countAllFiltered(filters: { status?: string[] }): Promise<number>;
   abstract countTypesInscriptions(id: string): Promise<number>;
   abstract incrementQuantityParticipants(
@@ -34,14 +51,6 @@ export abstract class EventGateway {
     id: string,
     quantity: number,
   ): Promise<Event>;
-  abstract findAllCarousel(): Promise<
-    {
-      id: string;
-      name: string;
-      location: string;
-      imageUrl: string;
-    }[]
-  >;
   abstract incrementAmountCollected(id: string, value: number): Promise<Event>;
   abstract decrementAmountCollected(id: string, value: number): Promise<Event>;
 
