29475d92857f7d1a64f20eb0b20334424c984da9
matheus souza <matheusfurtadogg@gmail.com>
2026-06-11 15:02:29 -0300

    refactor(expense): generaliza exceção de limite de imagens

--- src/infra/web/filters/usecases/image-limit-exceeded.usecase.exception.filter.ts
+++ src/infra/web/filters/usecases/image-limit-exceeded.usecase.exception.filter.ts
@@ -8,13 +8,13 @@ import { APP_FILTER } from '@nestjs/core';
 import { Response } from 'express';
 import { ExceptionUtils } from 'src/shared/utils/exception-utils';
 import { LogUtils } from 'src/shared/utils/log-utils';
-import { ReceiptsLimitExceededUsecaseException } from 'src/usecases/web/exceptions/expense/receipts-limit-exceeded.usecase.exception';
+import { ImageLimitExceededUsecaseException } from 'src/usecases/web/exceptions/image-limit-exceeded.usecase.exception';
 
-@Catch(ReceiptsLimitExceededUsecaseException)
-export class ReceiptsLimitExceededUsecaseExceptionFilter
+@Catch(ImageLimitExceededUsecaseException)
+export class ImageLimitExceededUsecaseExceptionFilter
   implements ExceptionFilter
 {
-  catch(exception: ReceiptsLimitExceededUsecaseException, host: ArgumentsHost) {
+  catch(exception: ImageLimitExceededUsecaseException, host: ArgumentsHost) {
     LogUtils.logException(exception);
 
     const ctx = host.switchToHttp();
@@ -26,7 +26,7 @@ export class ReceiptsLimitExceededUsecaseExceptionFilter
     response.status(status).json(aResponseData);
   }
 }
-export const ReceiptsLimitExceededUsecaseExceptionFilterProvider = {
+export const ImageLimitExceededUsecaseExceptionFilterProvider = {
   provide: APP_FILTER,
-  useClass: ReceiptsLimitExceededUsecaseExceptionFilter,
+  useClass: ImageLimitExceededUsecaseExceptionFilter,
 };
